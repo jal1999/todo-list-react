98f933b5920576b2129c159e1ea9e3637951263e
lafarrjam99 <58450211+lafarrjam99@users.noreply.github.com>
2022-01-03 00:49:03 -0500

    Removed console.log()'s.

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -4,14 +4,12 @@ import styles from './Tasks.module.css';
 const Tasks = props => {
 
     const onDelete = event => {
-        console.log(event);
         props.onDelete(event.target.getAttribute("att"));
     }
 
     return (
         <ul className={styles.list}>
             {props.tasks.map((task) => {
-                console.log("Mapped")
                 return (
                     <div className={styles.itemContainer}>
                         <li key={task} className={styles.listItem}>{task}</li>
